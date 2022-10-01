0344be336be85267e5bb0f51d018babd90154355
Didymus <e0544195@u.nus.edu>
2022-10-01 18:19:47 +0800

    Set up frontend repo

---
+++ frontend/pages/_app.js
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+
+/**
+ * Contains the logic for the whole app.
+ * @param {*} param0 
+ */
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ frontend/pages/login/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./index.module.scss";
+
+const LoginPage: React.FC = () => {
+  return <div className={styles.container}>Login Page</div>;
+};
+
+export default LoginPage;
