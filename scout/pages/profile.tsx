9824cda4e866f011138b042209ae36ccc4b14c22
Didymus <e0544195@u.nus.edu>
2022-10-07 17:16:57 +0800

    Add profile form

---
+++ scout/pages/profile.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "@chakra-ui/react";
+import ProfileForm from "../components/ProfileForm";
+
+const Profile = () => {
+  return (
+    <Container>
+      <ProfileForm />
+    </Container>
+  );
+};
+
+export default Profile;
