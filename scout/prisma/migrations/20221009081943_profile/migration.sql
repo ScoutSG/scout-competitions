5414b8318ccb83b4e8b51b4a19d39ba1be66824f
Keithc-G <ganhaokeith@gmail.com>
2022-10-10 00:28:11 +0800

    Fix application API

---
+++ scout/prisma/migrations/20221009081943_profile/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "gitHubUrl" TEXT,
+ADD COLUMN     "linkedinUrl" TEXT,
+ADD COLUMN     "major" TEXT,
+ADD COLUMN     "specialisation" TEXT,
+ADD COLUMN     "yearOfStudy" INTEGER;
