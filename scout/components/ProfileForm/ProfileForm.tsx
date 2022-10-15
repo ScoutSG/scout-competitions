3ae3da4aea4d7ed7b8f0aa62304fce0deffa9a6e
xlzior <wenjun.lye@gmail.com>
2022-10-15 19:34:19 +0800

    Add Telegram handle to profile form

--- scout/components/ProfileForm/ProfileForm.tsx
+++ scout/components/ProfileForm/ProfileForm.tsx
@@ -4,7 +4,7 @@ import {
   Heading,
   Text,
   Button,
-  VStack,
+  Stack,
   Wrap,
   WrapItem,
   FormControl,
@@ -13,19 +13,29 @@ import {
   InputGroup,
   InputLeftElement,
   NumberInput,
+  CircularProgress,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
   NumberInputField,
+  NumberInputStepper,
 } from "@chakra-ui/react";
-import { MdOutlineEmail } from "react-icons/md";
-import { BsGithub, BsPerson, BsLinkedin, BsTelegram } from "react-icons/bs";
-import { ChevronRightIcon } from "@chakra-ui/icons";
+import {
+  TbBrandGithub,
+  TbBrandLinkedin,
+  TbBrandTelegram,
+  TbChevronRight,
+  TbMail,
+  TbUser,
+} from "react-icons/tb";
 import clientApi from "../../core/api/client";
 import { useCustomToast } from "../../lib/hooks/useCustomToast";
+import { useForm } from "react-hook-form";
 import Loading from "../Loading";
 
 interface Profile {
   id: string | null;
   name: string | null;
-  yearOfStudy: string | null;
+  yearOfStudy: number | null;
   email: string | null;
   major: string | null;
   specialisation: string | null;
@@ -34,227 +44,165 @@ interface Profile {
   telegramUrl: string | null;
 }
 
-const ProfileForm = ({ displayAll }: { displayAll?: boolean }) => {
-  const [profile, setProfile] = useState<Profile | null>(null);
+const ProfileForm = () => {
+  const {
+    setValue,
+    reset,
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm<Profile>();
+  const [isLoading, setIsLoading] = useState(true);
   const { presentToast } = useCustomToast();
 
   useEffect(() => {
     async function getProfileDetails() {
       const response = await clientApi.get("/profile");
-
-      setProfile(response.data);
+      reset(response.data);
+      setIsLoading(false);
     }
 
     getProfileDetails();
   }, []);
 
-  const updateProfile = async () => {
-    const body = {
-      ...profile,
-      yearOfStudy: parseInt(profile.yearOfStudy) || null,
+  const onSubmit = async (values: Profile) => {
+    const updatedProfile = {
+      ...values,
+      telegramUrl:
+        values.telegramUrl[0] === "@"
+          ? values.telegramUrl.slice(1)
+          : values.telegramUrl,
     };
-
-    await clientApi
-      .patch("/profile", body)
-      .then((res) => {
-        presentToast({
-          title: "Successfully updated your profile!",
-          status: "success",
-          position: "top",
-        });
-      })
-      .catch((err) => {
-        presentToast({
-          title: "Can't update your profile details",
-          position: "top",
-          description: "Please check that your field entries are accurate",
-          status: "error",
-        });
+    try {
+      await clientApi.patch("/profile", updatedProfile);
+      presentToast({
+        title: "Successfully updated your profile!",
+        status: "success",
+        position: "top",
       });
+    } catch (err) {
+      presentToast({
+        title: "Can't update your profile details",
+        position: "top",
+        description: "Please check that you've entered the right details",
+        status: "error",
+      });
+    }
   };
 
-  return profile === null ? (
+  return isLoading ? (
     <Loading />
   ) : (
-    <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
-      <WrapItem>
-        <Box bg="white" borderRadius="lg">
-          <Box>
-            <Heading fontSize="md" fontWeight="semibold">
-              Your Details
-            </Heading>
-            <Text mt={{ sm: 3, md: 3, lg: 5 }} color="gray.500">
-              Update your profile details so that your group can easily contact
-              you.
-            </Text>
-          </Box>
-          <Box m={8}>
-            <VStack spacing={5}>
-              <FormControl id="name">
-                <FormLabel>Your Name</FormLabel>
-                <InputGroup>
-                  <InputLeftElement
-                    pointerEvents="none"
-                    children={<BsPerson color="gray.800" />}
-                  />
-                  <Input
-                    type="text"
-                    size="md"
-                    placeholder={"Write your name"}
-                    value={profile.name}
-                    onChange={(event) =>
-                      setProfile({
-                        ...profile,
-                        name: event.target.value,
-                      })
-                    }
-                  />
-                </InputGroup>
-              </FormControl>
-              <FormControl id="email">
-                <FormLabel>Email</FormLabel>
-                <InputGroup>
-                  <InputLeftElement
-                    pointerEvents="none"
-                    children={<MdOutlineEmail color="gray.800" />}
-                  />
-                  <Input
-                    type="text"
-                    size="md"
-                    value={profile.email}
-                    onChange={(event) =>
-                      setProfile({ ...profile, email: event.target.value })
-                    }
-                  />
-                </InputGroup>
-              </FormControl>
-              <FormControl id="year">
-                <FormLabel>Year of Study</FormLabel>
-                <NumberInput
-                  value={
-                    profile.yearOfStudy === null ? "" : profile.yearOfStudy
-                  }
-                >
-                  <NumberInputField
-                    placeholder="If you're Year 4, enter 4"
-                    onChange={(event) =>
-                      setProfile({
-                        ...profile,
-                        yearOfStudy: event.target.value,
-                      })
-                    }
-                  />
-                </NumberInput>
-              </FormControl>
-              <FormControl id="major">
-                <FormLabel>Major</FormLabel>
-                <Input
-                  type="text"
-                  size="md"
-                  placeholder="e.g. Computer Science, Business"
-                  value={profile.major}
-                  onChange={(event) =>
-                    setProfile({ ...profile, major: event.target.value })
-                  }
-                />
-              </FormControl>
-              <FormControl id="specialization">
-                <FormLabel>Speicalization</FormLabel>
-                <Input
-                  type="text"
-                  size="md"
-                  placeholder="e.g. Finance"
-                  value={profile.specialisation}
-                  onChange={(event) =>
-                    setProfile({
-                      ...profile,
-                      specialisation: event.target.value,
-                    })
-                  }
-                />
-              </FormControl>
-              <FormControl id="linkedin">
-                <FormLabel>LinkedIn URL</FormLabel>
-                <InputGroup>
-                  <InputLeftElement
-                    pointerEvents="none"
-                    children={<BsLinkedin color="gray.800" />}
-                  />
-                  <Input
-                    type="text"
-                    size="md"
-                    placeholder="https://www.linkedin.com/in/johndoe"
-                    value={profile.linkedinUrl}
-                    onChange={(event) =>
-                      setProfile({
-                        ...profile,
-                        linkedinUrl: event.target.value,
-                      })
-                    }
-                  />
-                </InputGroup>
-              </FormControl>
-              <FormControl id="GitHub">
-                <FormLabel>GitHub URL</FormLabel>
-                <InputGroup>
-                  <InputLeftElement
-                    pointerEvents="none"
-                    children={<BsGithub color="gray.800" />}
-                  />
-                  <Input
-                    type="text"
-                    size="md"
-                    placeholder="https://www.github.com/johndoe"
-                    value={profile.gitHubUrl}
-                    onChange={(event) =>
-                      setProfile({
-                        ...profile,
-                        gitHubUrl: event.target.value,
-                      })
-                    }
-                  />
-                </InputGroup>
-              </FormControl>
-              <FormControl id="Telegram">
-                <FormLabel>Telegram Username</FormLabel>
-                <InputGroup>
-                  <InputLeftElement
-                    pointerEvents="none"
-                    children={<BsTelegram color="gray.800" />}
-                  />
-                  <Input
-                    type="text"
-                    size="md"
-                    placeholder="@username"
-                    value={profile.telegramUrl}
-                    onChange={(event) =>
-                      setProfile({
-                        ...profile,
-                        telegramUrl: event.target.value,
-                      })
-                    }
-                  />
-                </InputGroup>
-              </FormControl>
-              <FormControl id="name" float="right">
-                <Button
-                  bgColor="primary.500"
-                  color="white"
-                  _hover={{
-                    bgColor: "gray.50",
-                    color: "primary.500",
-                  }}
-                  rightIcon={<ChevronRightIcon />}
-                  onClick={updateProfile}
-                >
-                  Update
-                </Button>
-              </FormControl>
-            </VStack>
-          </Box>
-        </Box>
-      </WrapItem>
-    </Wrap>
+    <Stack as="form" onSubmit={handleSubmit(onSubmit)} spacing={5}>
+      <Box>
+        <Heading fontSize="md" fontWeight="semibold">
+          Your Details
+        </Heading>
+        <Text mt={3} color="gray.500">
+          Update your profile details so that your group can easily contact you.
+        </Text>
+      </Box>
+      <FormControl id="name">
+        <FormLabel>Your Name</FormLabel>
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents="none"
+            children={<TbUser color="gray.800" />}
+          />
+          <Input placeholder="Write your name" {...register("name")} />
+        </InputGroup>
+      </FormControl>
+      <FormControl id="email">
+        <FormLabel>Email</FormLabel>
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents="none"
+            children={<TbMail color="gray.800" />}
+          />
+          <Input type="email" {...register("email")} />
+        </InputGroup>
+      </FormControl>
+      <FormControl id="year">
+        <FormLabel>Year of Study</FormLabel>
+        <NumberInput
+          placeholder="If you're Year 4, enter 4"
+          {...register("yearOfStudy", { valueAsNumber: true })}
+          onChange={(valueString) =>
+            setValue("yearOfStudy", parseInt(valueString))
+          }
+          min={Number.MIN_SAFE_INTEGER}
+          max={Number.MAX_SAFE_INTEGER}
+        >
+          <NumberInputField name="targetSize" />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+      </FormControl>
+      <FormControl id="major">
+        <FormLabel>Major</FormLabel>
+        <Input
+          placeholder="e.g. Computer Science, Business"
+          {...register("major")}
+        />
+      </FormControl>
+      <FormControl id="specialization">
+        <FormLabel>Speicalization</FormLabel>
+        <Input placeholder="e.g. Finance" {...register("specialisation")} />
+      </FormControl>
+      <FormControl id="linkedin">
+        <FormLabel>LinkedIn URL</FormLabel>
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents="none"
+            children={<TbBrandLinkedin color="gray.800" />}
+          />
+          <Input
+            placeholder="https://www.linkedin.com/in/johndoe"
+            {...register("linkedinUrl")}
+          />
+        </InputGroup>
+      </FormControl>
+      <FormControl id="GitHub">
+        <FormLabel>GitHub URL</FormLabel>
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents="none"
+            children={<TbBrandGithub color="gray.800" />}
+          />
+          <Input
+            placeholder="https://www.github.com/johndoe"
+            {...register("gitHubUrl")}
+          />
+        </InputGroup>
+      </FormControl>
+      <FormControl id="telegram">
+        <FormLabel>Telegram Username</FormLabel>
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents="none"
+            children={<TbBrandTelegram color="gray.800" />}
+          />
+          <Input placeholder="username" {...register("telegramUrl")} />
+        </InputGroup>
+      </FormControl>
+      <Button
+        bgColor="primary.500"
+        color="white"
+        _hover={{
+          bgColor: "gray.50",
+          color: "primary.500",
+        }}
+        rightIcon={<TbChevronRight />}
+        type="submit"
+        maxWidth="fit-content"
+        isLoading={isSubmitting}
+        alignSelf="flex-end"
+      >
+        Update
+      </Button>
+    </Stack>
   );
 };
 
