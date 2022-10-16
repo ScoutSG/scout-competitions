0ae837b337dce0b0c7dcfc80d1840c18a325223e
xlzior <wenjun.lye@gmail.com>
2022-10-16 14:48:01 +0800

    Add telegram utils

---
+++ scout/core/utils/telegram.ts
@@ -0,0 +1,81 @@
+import { Api, TelegramClient } from "telegram";
+import { StringSession } from "telegram/sessions";
+
+const stringSession = process.env.ACCOUNT_SESSION_ID;
+const API_ID = parseInt(process.env.API_ID);
+const API_HASH = process.env.API_HASH;
+
+const client = new TelegramClient(
+  new StringSession(stringSession),
+  API_ID,
+  API_HASH,
+  { connectionRetries: 1 }
+);
+
+const toBigInt = (num: number | string): bigInt.BigInteger => {
+  return BigInt(num) as unknown as bigInt.BigInteger;
+};
+
+const generateRandomId = () => {
+  return Math.floor(Math.random() * 999999999999999);
+};
+
+export const createGroup = async (title: string, userId: string) => {
+  await client.connect();
+  try {
+    console.log("daheckie");
+    console.log(["me", userId]);
+    const result = (await client.invoke(
+      new Api.messages.CreateChat({
+        users: ["me", userId],
+        title,
+      })
+    )) as Api.Updates;
+    const chatId = result.chats[0].id.toJSNumber();
+    await client.invoke(
+      new Api.messages.EditChatAdmin({
+        chatId: toBigInt(chatId),
+        userId,
+        isAdmin: true,
+      })
+    );
+    return chatId;
+  } catch (err) {
+    if (err.errorMessage === "USERS_TOO_FEW") {
+      return client.invoke(
+        new Api.messages.SendMessage({
+          peer: userId,
+          message:
+            "Hello from ScoutSG! I wasn't able to add you to a group chat due to your privacy settings.\nCould you add @scoutsg as a contact? Then I'll be able to add you to a Telegram group!\nAlternatively, you could also add @scoutsg as an exception under Privacy and Security > Groups & Channels.",
+          randomId: toBigInt(generateRandomId()),
+        })
+      );
+    }
+    throw new Error(
+      "Please add @scoutsg as a contact on Telegram so that we can set up a Telegram group for you!"
+    );
+  }
+};
+
+export const addToGroup = async (groupId: string, userId: string) => {
+  await client.connect();
+  const result = await client.invoke(
+    new Api.messages.AddChatUser({
+      chatId: toBigInt(groupId),
+      userId,
+    })
+  );
+  console.log(result);
+};
+
+export const notifyGroup = async (groupId: string, message: string) => {
+  await client.connect();
+  const result = await client.invoke(
+    new Api.messages.SendMessage({
+      peer: new Api.InputPeerChat({ chatId: toBigInt(groupId) }),
+      message,
+      randomId: toBigInt(generateRandomId()),
+    })
+  );
+  console.log(result);
+};
