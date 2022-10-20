a4deeaab078456301c15fbd2e614ffbc44dac54a
Didymus <e0544195@u.nus.edu>
2022-10-20 23:55:03 +0800

    Add connection between front and back to autojoin group

--- scout/pages/join/[code].tsx
+++ scout/pages/join/[code].tsx
@@ -1,5 +1,9 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
+import clientApi from "../../core/api/client";
+import { useCustomToast } from "../../lib/hooks/useCustomToast";
+import { useJoinRequest } from "../../lib/hooks/useJoinRequest";
 
 interface Invite {
   code: string;
@@ -7,10 +11,30 @@ interface Invite {
 }
 
 const JoinPage = () => {
-  // call to join on mount
+  const router = useRouter();
+  const session = useSession();
+  const { presentToast } = useCustomToast();
+  const { setJoinRequest } = useJoinRequest();
+
   useEffect(() => {
-    // create application
-    // approve application
+    if (!session.data || !session.data.user) {
+      setJoinRequest({
+        id: router.query.code,
+      });
+      router.push("/auth/signin");
+      presentToast({
+        title: "Almost there! Login to to join.",
+        position: "top",
+        status: "info",
+      });
+    } else {
+      let code = router.query.code;
+      const body = {
+        userId: session.data.user.id,
+      };
+
+      clientApi.patch(`/invitations/${code}`, body);
+    }
   }, []);
 
   return <div>Test</div>;
