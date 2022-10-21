cdd43773352052e2af81450ffd63e37c0ecaaadc
Keithc-G <ganhaokeith@gmail.com>
2022-10-22 01:30:22 +0800

    Fix authentication issue in isMember hook

--- scout/components/CompetitionDetails/useIsMember.tsx
+++ scout/components/CompetitionDetails/useIsMember.tsx
@@ -5,15 +5,21 @@ function useIsMember(competitionId: number) {
   const fetcher = (arg: any, ...args: any[]) =>
     fetch(arg, ...args).then((res) => res.json());
   const { data: session } = useSession();
-  const { data, error } = useSWR(
-    `/api/groups/competition/${competitionId}/user/${session.user.id}`,
-    fetcher
-  );
-  return {
-    isMember: data && data.length > 0,
-    isLoading: !error && !data,
-    isError: error,
-  };
+  if (session) {
+    const { data, error } = useSWR(
+      `/api/groups/competition/${competitionId}/user/${session.user.id}`,
+      fetcher
+    );
+    return {
+      isMember: data && data.length > 0,
+      isLoading: !error && !data,
+      isError: error,
+    };
+  } else {
+    return {
+      isMember: false
+    }
+  }
 }
 
 export default useIsMember;
