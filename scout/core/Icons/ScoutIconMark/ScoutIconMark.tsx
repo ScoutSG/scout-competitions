e29875b092467730ecaac41fcb3cb58480c0e0c4
Didymus <e0544195@u.nus.edu>
2022-10-24 17:21:28 +0800

    Redesign loading component

--- scout/core/Icons/ScoutIconMark/ScoutIconMark.tsx
+++ scout/core/Icons/ScoutIconMark/ScoutIconMark.tsx
@@ -2,7 +2,13 @@ import { chakra, Box } from "@chakra-ui/react";
 import logoMark from "../../assets/logo-mark.svg";
 import Image from "next/image";
 
-const ScoutIconMark: React.FC = () => {
+const ScoutIconMark: React.FC = ({
+  width = "100px",
+  height = "100px",
+}: {
+  width: string;
+  height: string;
+}) => {
   const ProductImage = chakra(Image, {
     shouldForwardProp: (prop) =>
       ["width", "height", "src", "alt", "layout", "sizes"].includes(prop),
@@ -10,7 +16,13 @@ const ScoutIconMark: React.FC = () => {
 
   return (
     <>
-      <ProductImage src={logoMark} alt="logo-mark" />
+      <ProductImage
+        src={logoMark}
+        alt="logo-mark"
+        width={width}
+        height={height}
+        animation={""}
+      />
     </>
   );
 };
