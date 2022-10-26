ee34452fe0de9e6499d03458321dd2e018ccaaee
xlzior <wenjun.lye@gmail.com>
2022-10-26 16:36:35 +0800

    Linkify competition description

--- scout/pages/_app.tsx
+++ scout/pages/_app.tsx
@@ -15,6 +15,7 @@ import "@fontsource/open-sans/500.css";
 import "@fontsource/open-sans/600.css";
 import "@fontsource/open-sans/700.css";
 import "@fontsource/open-sans/800.css";
+import "../core/styles/global.scss";
 
 if (process.env.NEXT_PUBLIC_APP_TRACKING_ID) {
   ReactGA.initialize(process.env.NEXT_PUBLIC_APP_TRACKING_ID);
